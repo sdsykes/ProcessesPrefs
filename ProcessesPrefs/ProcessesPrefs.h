56e83be4e8945991b075f7dadb5881f68c6f9596
Stephen Sykes <sdsykes@gmail.com>
2016-01-27 00:15:15 +0200

    Initial Commit

---
+++ ProcessesPrefs/ProcessesPrefs.h
@@ -0,0 +1,15 @@
+//
+//  ProcessesPrefs.h
+//  ProcessesPrefs
+//
+//  Created by Stephen Sykes on 27/01/16.
+//  Copyright © 2016 Switchstep. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface ProcessesPrefs : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ ProcessesPrefs/ProcessesPrefs.m
@@ -0,0 +1,17 @@
+//
+//  ProcessesPrefs.m
+//  ProcessesPrefs
+//
+//  Created by Stephen Sykes on 27/01/16.
+//  Copyright © 2016 Switchstep. All rights reserved.
+//
+
+#import "ProcessesPrefs.h"
+
+@implementation ProcessesPrefs
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
