9d7d54619de4e3a09328b4e9cee9e42c4e69e8ee
Stephen Sykes <sdsykes@gmail.com>
2016-01-27 20:26:25 +0200

    First

--- ProcessesPrefs/ProcessesPrefs.m
+++ ProcessesPrefs/ProcessesPrefs.m
@@ -8,10 +8,41 @@
 
 #import "ProcessesPrefs.h"
 
+NSString *const kAllProcesses = @"allProcesses";
+NSString *const kAllProcessesNotifcationName = @"com.switchstep.allProcessesNotification";
+NSString *const kTheApp = @"com.switchstep.Processes";
+
+@interface ProcessesPrefs ()
+
+@property (nonatomic, weak) IBOutlet NSButton *allProcesses;
+@property (nonatomic) NSUserDefaults *defaults;
+
+@end
+
 @implementation ProcessesPrefs
 
 - (void)mainViewDidLoad
 {
+    // Can get the prefs via NSUserDefaults
+    NSDictionary *defaults = [[NSUserDefaults standardUserDefaults] persistentDomainForName:kTheApp];
+    BOOL allProcessesDefault = [[defaults objectForKey:@"allProcesses"] boolValue];
+
+    self.allProcesses.state = allProcessesDefault ? NSOnState : NSOffState;
+}
+
+
+- (IBAction)allProcessesChanged:(NSButton *)sender
+{
+    // Have to set the prefs using Carbon
+    if (sender.state == NSOnState) {
+        CFPreferencesSetAppValue((CFStringRef)kAllProcesses, kCFBooleanTrue, (CFStringRef)kTheApp);
+        CFPreferencesAppSynchronize((CFStringRef)kTheApp);
+        [[NSDistributedNotificationCenter defaultCenter] postNotificationName:kAllProcessesNotifcationName object:@"YES" userInfo:nil deliverImmediately:YES];
+    } else {
+        CFPreferencesSetAppValue((CFStringRef)kAllProcesses, kCFBooleanFalse, (CFStringRef)kTheApp);
+        CFPreferencesAppSynchronize((CFStringRef)kTheApp);
+        [[NSDistributedNotificationCenter defaultCenter] postNotificationName:kAllProcessesNotifcationName object:@"NO" userInfo:nil deliverImmediately:YES];
+    }
 }
 
 @end
---
+++ README.md
@@ -0,0 +1,9 @@
+# ProcessesPrefs
+
+## This is just a demo
+
+Sets the prefs for the [Processes App](https://github.com/sdsykes/Processes).
+
+The build script installs this in ~/Library/PreferencePanes. So just build it on your machine and it should be ready to go.
+
+In a real app you'd bundle it and install the pref pane when the app starts for the first time. TotalSpaces has code like that to install and update the plugin component.
